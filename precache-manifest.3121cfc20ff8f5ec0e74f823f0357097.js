a57ac69b3bf567da7cb95fa969fa9bb56f0ae26a
Aleksi Tarvainen <a.aleksi.tarvainen@gmail.com>
2020-03-22 13:32:09 +0200

    Updates

---
+++ precache-manifest.3121cfc20ff8f5ec0e74f823f0357097.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63080efbeb465d5702b8c70571c4ca52",
+    "url": "/rpg-dice/index.html"
+  },
+  {
+    "revision": "67001914a94d6538b531",
+    "url": "/rpg-dice/static/css/main.8c2366ba.chunk.css"
+  },
+  {
+    "revision": "c2a6b9d7d00ee090eb90",
+    "url": "/rpg-dice/static/js/2.1fdc582c.chunk.js"
+  },
+  {
+    "revision": "67001914a94d6538b531",
+    "url": "/rpg-dice/static/js/main.8d9373e9.chunk.js"
+  },
+  {
+    "revision": "c417142de9ca00c94f04",
+    "url": "/rpg-dice/static/js/runtime~main.301f3d4c.js"
+  }
+]);
